06314b71c28ad2c1657aeee6a134b3dbceab77a3
Pifpafplouff <gaonachalan@gmail.com>
2025-04-09 14:30:26 +0200

    add stuff

---
+++ wp-content/themes/twentytwentyone-child/template_parts/header/modal-contact.php
@@ -0,0 +1,21 @@
+<!-- Trigger/Open The Modal 
+ <button id="myBtn">Open Modal</button> -->
+
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <div class="modal-title-wrapper">
+      <h1 class="modal-title">
+      CONTACT
+      </h1>
+        <span class="close">&times;</span>
+    </div>
+        <?php
+        echo do_shortcode('[contact-form-7 id="104e5d2" title="Contactez-moi"]');
+        ?>
+  </div>
+
+</div>
